6ebaee0e3cb4fdd172ab9162ebd8c0d5ce7031ba
rebyul <donghan1991@hotmail.com>
2016-06-13 18:08:38 +1200

    Add properties for user model

--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -1,3 +1,12 @@
 export interface User {
-    
+    Id: number;
+    FirstName: string;
+    Lastname: string;
+    Email: string;
+    AvatarUrl: string;
+    Shopper: boolean;
+    BankDetails: string;
+    PhoneNumber: string;
+    BankName: string;
+    FirstLogin: boolean;
 }
