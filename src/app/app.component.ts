0fd084f9efa6ada035a2fa3f376a7b2ff2c94547
Andrew Hughson <andrew@hughson.me>
2017-01-26 11:12:23 +1300

    Update angular-cli version and dependencies (#8)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import 'rxjs/add/operator/map';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'sc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit {
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
+export class AppComponent {
 }
